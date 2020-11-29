0047384ed25442a98aab1f5403d9050c5c1136dc
Bintang Fadjrul Fallah <bintangfadjrulfallah@gmail.com>
2020-11-30 00:46:10 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({allPostsData}) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Hello, I'm <b>Bintang</b></p>
+                <p>Hello, I'm <b>Bintangg</b></p>
                 <p>
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
